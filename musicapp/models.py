d3865dc5be59ed5f7cccbb43c5d2d52173056fef
bismarkboateng <bismarkb609@gmail.com>
2022-10-28 21:50:03 +0000

    model created

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    date_released = models.DateTimeField()
+    likes = models.CharField(max_length=50)
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE) 
+
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song = models.ForeignKey(Song, on_delete=models.SET_NULL)
